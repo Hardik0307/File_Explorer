b0db994d31c7c8cb4d5c5a29e414c0d9d0713b5a
Hardik <bagadahardik2000@gmail.com>
2019-12-30 21:13:04 +0530

    Modified - Imported Packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:flutter_file_manager/flutter_file_manager.dart';
+import 'package:open_file/open_file.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:simple_permissions/simple_permissions.dart';
+
 
 var logger = Logger(
   printer: PrettyPrinter(),
