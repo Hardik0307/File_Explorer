f6c3830d68dde007de535cf55dc24872570774aa
Harshad Chovatiya <harshadchovatiya9@gmail.com>
2019-12-30 17:46:51 +0530

    changed main File

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'File Explorer'),
     );
