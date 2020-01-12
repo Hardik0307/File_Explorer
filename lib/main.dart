03a185792d6f897c40f8f7d61c7a3c602ce99c5d
Harshad Chovatiya <harshadchovatiya9@gmail.com>
2020-01-12 18:30:58 +0530

    Initialize main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:flutter_file_manager/flutter_file_manager.dart';
-import 'package:open_file/open_file.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-//import 'package:simple_permissions/simple_permissions.dart';
-
-
-var logger = Logger(
-  printer: PrettyPrinter(),
-);
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'File Explorer',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(title: 'File Explorer'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-
-  
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Files',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('File Explorer'),
+        ),
+        body: Center(
+          child: Text('Work in Progress.......!'),
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
